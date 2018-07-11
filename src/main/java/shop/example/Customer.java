396d416a1c5af262db262ee6066869663ff7c708
Abhinav Manchanda <abhinav.manchanda@gmail.com>
2018-07-11 20:30:45 +0530

    Renamed a few variables, added logic for delivery charges for greater quantities.

--- src/main/java/shop/example/Customer.java
+++ src/main/java/shop/example/Customer.java
@@ -30,28 +30,37 @@ public class Customer {
 
     public String dailyOrderSummary(){
         double totalAmount = 0;
-        String orderSummary = "Here is your order summary for the day \n";
+        String sm = "Dear "+ name +", Here is your order summary for the day \n";
 
-        for(PurchaseOrder purchaseOrder : purchaseOrders){
-            if(removeTime(purchaseOrder.getDate()).equals(removeTime(Calendar.getInstance().getTime()))){
-                Product product = purchaseOrder.getProduct();
-                orderSummary += "Product: " + product.productDetails() + "\n";
-                orderSummary += "Quantity: " + purchaseOrder.getQuantity() + "\n";
-                totalAmount += product.getPrice() * purchaseOrder.getQuantity();
+        for(PurchaseOrder ord : purchaseOrders){
+            if(removeTime(ord.getDate()).equals(removeTime(Calendar.getInstance().getTime()))){
+                Product product = ord.getProduct();
+                sm += "..........\n";
+                sm += "Product: " + product.productDetails() + "\n";
+                sm += "Quantity: " + ord.getQuantity() + "\n";
+                sm += "Total Cost of Product: " + product.getPrice() * ord.getQuantity() + "\n";
+                totalAmount += product.getPrice() * ord.getQuantity();
                 if(product.getProductType()==2){
-                    orderSummary += " Delivery Charges for this product: " + 20 + "\n" + "\n";
+                    sm += " Delivery Charges for this product: " + 20 + "\n";
                     totalAmount += 20;
                 } else{
-                    orderSummary += " Delivery Charges for this product: " + 30 + "\n" + "\n";
-                    totalAmount += 30;
+                    if(ord.getQuantity() >= 5){
+                        sm += " Delivery Charges for this product: " + 50 + "\n";
+                        totalAmount += 50;
+                    } else {
+                        sm += " Delivery Charges for this product: " + 30 + "\n";
+                        totalAmount += 30;
+                    }
                 }
             }
         }
 
-        orderSummary += " Your total amount: " + totalAmount + "\n";
-        orderSummary += " Kindly pay cash to the delivery officer." + "\n" + "\n";
-        orderSummary += "-------------\n\n";
-        return orderSummary;
+        sm += "..........\n";
+        sm += " Your total amount: " + totalAmount + "\n";
+        sm += " Your registered address is: " + address + "\n" ;
+        sm += " Kindly pay cash to the delivery officer." + "\n" ;
+        sm += "-------------\n\n\n";
+        return sm;
     }
 
     public Date removeTime(Date date) {
